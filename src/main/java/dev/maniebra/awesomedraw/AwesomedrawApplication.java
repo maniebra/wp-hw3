32f40675f2e4eefd75178c62c5066342162df78b
maniebra <mani.ebra@gmail.com>
2025-07-29 20:15:28 +0330

    init

---
+++ src/main/java/dev/maniebra/awesomedraw/AwesomedrawApplication.java
@@ -0,0 +1,13 @@
+package dev.maniebra.awesomedraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomedrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomedrawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/maniebra/awesomedraw/AwesomedrawApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.maniebra.awesomedraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomedrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
