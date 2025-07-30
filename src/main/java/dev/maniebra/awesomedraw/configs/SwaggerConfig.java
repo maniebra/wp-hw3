fd821bfb1b13096a8347ce922eae377fa4de71bf
maniebra <mani.ebra@gmail.com>
2025-07-30 10:49:30 +0330

    add configuration files

---
+++ src/main/java/dev/maniebra/awesomedraw/configs/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package dev.maniebra.awesomedraw.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("AwesomeDraw API")
+                        .description("AwesomeDraw's Swagger documentation")
+                        .version("1.0"));
+    }
+}
